ed1835968ece8594df40b5d61523923875a3a0ce
GARajab <mohamed.rajab@ewa.com>
2024-10-11 22:42:20 +0300

    all arrows working fine now i have array with the ids of player start now on pc

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // const
-const imgPlyr0 = document.getElementById("plrImg0")
-const imgPlyr1 = document.getElementById("plrImg1")
-const imgPlyr2 = document.getElementById("plrImg2")
-const imgPlyr3 = document.getElementById("plrImg3")
-const imgPlyr4 = document.getElementById("plrImg4")
-const imgPlyr5 = document.getElementById("plrImg5")
+// const imgPlyr0 = document.getElementById("plrImg0")
+// const imgPlyr1 = document.getElementById("plrImg1")
+// const imgPlyr2 = document.getElementById("plrImg2")
+// const imgPlyr3 = document.getElementById("plrImg3")
+// const imgPlyr4 = document.getElementById("plrImg4")
+// const imgPlyr5 = document.getElementById("plrImg5")
 
 const imgPc0 = document.getElementById("compImg0")
 const imgPc1 = document.getElementById("compImg1")
@@ -21,10 +21,10 @@ let initArray = [0, 0, 0, 0, 0, 0]
 let array = new Array(7).fill(null) // Create an array of length 7 filled with null values
 
 //? functions
-function changeImage(playerVal, playerImg) {
-  currentIndex = (currentIndex + 1) % images.length
-  playerImg.src = images[currentIndex]
-}
+// function changeImage(playerVal, playerImg) {
+//   currentIndex = (currentIndex + 1) % images.length
+//   playerImg.src = images[currentIndex]
+// }
 const images = [
   "/images/0.png",
   "/images/1.png",
@@ -33,107 +33,123 @@ const images = [
 ]
 
 // Current index
-let currentIndex = 0
-const filling = () => {
-  let newElements = playerArrowsDirections // Replace with your actual new elements
-  fillArray(newElements)
+// let currentIndex = 0
+// const filling = () => {
+//   let newElements = playerArrowsDirections // Replace with your actual new elements
+//   fillArray(newElements)
+// }
+// const fillArray = (newElements) => {
+//   array.splice(0, 7, ...newElements) // Replace the first 7 elements with the new elements
+//   // console.log(array)
+// }
+// const changePlayerArrowsDirectionsZero = () => {
+//   filling()
+//   const lZeroVal = imgPlyr0.getAttribute("value")
+
+//   if (playerArrowsDirections.length > 0) {
+//     playerArrowsDirections[0] = currentIndex
+//     if (playerArrowsDirections[0] === -1) {
+//       playerArrowsDirections[0] = 3
+//     }
+//   } else {
+//     playerArrowsDirections.push(currentIndex)
+//   }
+
+//   changeImage(lZeroVal, imgPlyr0)
+//   // console.log(images[currentIndex])
+// }
+// const changePlayerArrowsDirectionsOne = () => {
+//   filling()
+//   const lOneVal = imgPlyr1.getAttribute("value")
+
+//   if (playerArrowsDirections.length > 1) {
+//     playerArrowsDirections[1] = currentIndex
+//     if (playerArrowsDirections[1] === -1) {
+//       playerArrowsDirections[1] = 3
+//     }
+//   } else {
+//     playerArrowsDirections.push(currentIndex)
+//   }
+
+//   changeImage(lOneVal, imgPlyr1)
+//   // console.log(images[currentIndex])
+// }
+// const changePlayerArrowsDirectionsTwo = () => {
+//   filling()
+
+//   const lTwoVal = imgPlyr2.getAttribute("value")
+//   if (playerArrowsDirections.length > 2) {
+//     playerArrowsDirections[2] = currentIndex
+//     if (playerArrowsDirections[2] === -1) {
+//       playerArrowsDirections[2] = 3
+//     }
+//   } else {
+//     playerArrowsDirections.push(currentIndex)
+//   }
+
+//   changeImage(lTwoVal, imgPlyr2)
+//   // console.log(images[currentIndex])
+// }
+// const changePlayerArrowsDirectionsThree = () => {
+//   filling()
+//   const lThreeVal = imgPlyr3.getAttribute("value")
+//   if (playerArrowsDirections.length > 3) {
+//     playerArrowsDirections[3] = currentIndex
+//     if (playerArrowsDirections[3] === -1) {
+//       playerArrowsDirections[3] = 3
+//     }
+//   } else {
+//     playerArrowsDirections.push(currentIndex)
+//   }
+
+//   changeImage(lThreeVal, imgPlyr3)
+//   // console.log(images[currentIndex])
+// }
+// const changePlayerArrowsDirectionsFour = () => {
+//   filling()
+//   const lFourVal = imgPlyr4.getAttribute("value")
+//   if (playerArrowsDirections.length > 4) {
+//     playerArrowsDirections[4] = currentIndex
+//     if (playerArrowsDirections[4] === -1) {
+//       playerArrowsDirections[4] = 3
+//     }
+//   } else {
+//     playerArrowsDirections.push(currentIndex)
+//   }
+
+//   changeImage(lFourVal, imgPlyr4)
+//   // console.log(images[currentIndex])
+// }
+// const changePlayerArrowsDirectionsFive = () => {
+//   filling()
+//   const lFiveVal = imgPlyr5.getAttribute("value")
+//   if (playerArrowsDirections.length > 5) {
+//     playerArrowsDirections[5] = currentIndex
+//     if (playerArrowsDirections[5] === -1) {
+//       playerArrowsDirections[5] = 3
+//     }
+//   } else {
+//     playerArrowsDirections.push(currentIndex)
+//   }
+
+//   changeImage(lFiveVal, imgPlyr5)
+//   console.log(playerArrowsDirections)
+// }
+
+let currentIndices = Array(6).fill(0) // Initialize current indices for each image
+
+const changeImage = (index) => {
+  currentIndices[index] = (currentIndices[index] + 1) % images.length // Increment index and wrap around
+  const img = document.getElementById(`plrImg${index}`)
+  img.src = images[currentIndices[index]] // Change the src of the image
+  console.log(index)
+  console.log(currentIndices)
 }
-const fillArray = (newElements) => {
-  array.splice(0, 7, ...newElements) // Replace the first 7 elements with the new elements
-  // console.log(array)
-}
-const changePlayerArrowsDirectionsZero = () => {
-  filling()
-  const lZeroVal = imgPlyr0.getAttribute("value")
 
-  if (playerArrowsDirections.length <= 0) {
-    playerArrowsDirections[0] = currentIndex
-    if (playerArrowsDirections[0] === -1) {
-      playerArrowsDirections[0] = 3
-    }
-  } else {
-    playerArrowsDirections.push(currentIndex)
-  }
-
-  changeImage(lZeroVal, imgPlyr0)
-  // console.log(images[currentIndex])
-}
-const changePlayerArrowsDirectionsOne = () => {
-  filling()
-  const lOneVal = imgPlyr1.getAttribute("value")
-
-  if (playerArrowsDirections.length > 1) {
-    playerArrowsDirections[1] = currentIndex
-    if (playerArrowsDirections[1] === -1) {
-      playerArrowsDirections[1] = 3
-    }
-  } else {
-    playerArrowsDirections.push(currentIndex)
-  }
-
-  changeImage(lOneVal, imgPlyr1)
-  // console.log(images[currentIndex])
-}
-const changePlayerArrowsDirectionsTwo = () => {
-  filling()
-
-  const lTwoVal = imgPlyr2.getAttribute("value")
-  if (playerArrowsDirections.length > 2) {
-    playerArrowsDirections[2] = currentIndex
-    if (playerArrowsDirections[2] === -1) {
-      playerArrowsDirections[2] = 3
-    }
-  } else {
-    playerArrowsDirections.push(currentIndex)
-  }
-
-  changeImage(lTwoVal, imgPlyr2)
-  // console.log(images[currentIndex])
-}
-const changePlayerArrowsDirectionsThree = () => {
-  filling()
-  const lThreeVal = imgPlyr3.getAttribute("value")
-  if (playerArrowsDirections.length > 3) {
-    playerArrowsDirections[3] = currentIndex
-    if (playerArrowsDirections[3] === -1) {
-      playerArrowsDirections[3] = 3
-    }
-  } else {
-    playerArrowsDirections.push(currentIndex)
-  }
-
-  changeImage(lThreeVal, imgPlyr3)
-  // console.log(images[currentIndex])
-}
-const changePlayerArrowsDirectionsFour = () => {
-  filling()
-  const lFourVal = imgPlyr4.getAttribute("value")
-  if (playerArrowsDirections.length > 4) {
-    playerArrowsDirections[4] = currentIndex
-    if (playerArrowsDirections[4] === -1) {
-      playerArrowsDirections[4] = 3
-    }
-  } else {
-    playerArrowsDirections.push(currentIndex)
-  }
-
-  changeImage(lFourVal, imgPlyr4)
-  // console.log(images[currentIndex])
-}
-const changePlayerArrowsDirectionsFive = () => {
-  filling()
-  const lFiveVal = imgPlyr5.getAttribute("value")
-  if (playerArrowsDirections.length > 5) {
-    playerArrowsDirections[5] = currentIndex
-    if (playerArrowsDirections[5] === -1) {
-      playerArrowsDirections[5] = 3
-    }
-  } else {
-    playerArrowsDirections.push(currentIndex)
-  }
-
-  changeImage(lFiveVal, imgPlyr5)
-  console.log(playerArrowsDirections)
+// Add event listeners for each button
+for (let i = 0; i < 6; i++) {
+  const button = document.getElementById(`L${i}`)
+  button.addEventListener("click", () => changeImage(i))
 }
 
 const fillPlayerTriesArray = () => {}
@@ -189,40 +205,3 @@ const pcArrows = document.querySelectorAll(".pcImg0")
 startBtn.addEventListener("click", startGame)
 // resetEvent
 rstButton.addEventListener("click", initializeGame)
-// full 12 btn click event
-// buttons.forEach((button) => {
-//   button.addEventListener("click", changePlayerArrowsDirections)
-// })
-//? full 12 btn seperate cashing and lisiting
-const lZero = document.getElementById("L0")
-lZero.addEventListener("click", changePlayerArrowsDirectionsZero)
-// const Rzero = document.getElementById("R0")
-// Rzero.addEventListener("click", changePlayerArrowsDirections)
-const lOne = document.getElementById("L1")
-lOne.addEventListener("click", changePlayerArrowsDirectionsOne)
-// const Rone = document.getElementById("R1")
-// Rone.addEventListener("click", changePlayerArrowsDirections)
-const lTwo = document.getElementById("L2")
-lTwo.addEventListener("click", changePlayerArrowsDirectionsTwo)
-// const Rtwo = document.getElementById("R2")
-// Rtwo.addEventListener("click", changePlayerArrowsDirections)
-const lThree = document.getElementById("L3")
-lThree.addEventListener("click", changePlayerArrowsDirectionsThree)
-// const Rthree = document.getElementById("R3")
-// Rthree.addEventListener("click", changePlayerArrowsDirections)
-const lFour = document.getElementById("L4")
-lFour.addEventListener("click", changePlayerArrowsDirectionsFour)
-// const Rfour = document.getElementById("R4")
-// Rfour.addEventListener("click", changePlayerArrowsDirections)
-const lFive = document.getElementById("L5")
-lFive.addEventListener("click", changePlayerArrowsDirectionsFive)
-// const Rfive = document.getElementById("R5")
-// Rfive.addEventListener("click", changePlayerArrowsDirections)
-
-// full 6 images to change there src
-
-// test area
-
-// Assuming you have a button element with the id "myButton"
-
-// Array of images
